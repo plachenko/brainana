297f4e26b173dc5285d01802fd42fb4f17547f87
Chris Wood <drgrubber@gmail.com>
2025-12-27 21:31:08 -0700

    Changed text parsing to a markdown controller. Added react syntax highlighter. Added css to style h1-h6 components.

--- src/components/Wood/AI.jsx
+++ src/components/Wood/AI.jsx
@@ -1,10 +1,13 @@
 import { GoogleGenAI } from "@google/genai";
 import { useState, useRef, useEffect } from "react";
+import ReactMarkdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { dracula } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 export default function AI() {
   const [apiKey, setApiKey] = useState("");
   const [prompt, setPrompt] = useState("");
-  const [chatContents, setChatContents] = useState([]);
+  const [fullResponseText, setFullResponseText] = useState("");
   const thinkingRef = useRef(null);
   const containerRef = useRef(null);
 
@@ -12,16 +15,16 @@ export default function AI() {
     if (containerRef.current) {
       containerRef.current.scrollTop = containerRef.current.scrollHeight;
     }
-  }, [chatContents]);
+  }, [fullResponseText]);
 
-  // To run this code you need to install the following dependencies:
-  // npm install @google/genai mime
-  // npm install -D @types/node
-  async function main() {
+  async function handleSubmit(e) {
+    e.preventDefault();
+
     if (apiKey.trim() === "" || prompt.trim() === "") {
       return;
     }
 
+    //Show thinking div
     containerRef.current.classList.replace("special-flex", "hidden");
     thinkingRef.current.classList.replace("hidden", "flex");
 
@@ -71,7 +74,7 @@ export default function AI() {
 
         if (text === "") {
           if (newText !== "") {
-            newText += "\n";
+            newText += "\n\n";
           }
           continue;
         }
@@ -80,49 +83,27 @@ export default function AI() {
       }
 
       console.log(newText);
-      //replace double **Word** with bold
-      newText = newText.replace(/\*\*(.*?)\*\*/g, "<b>$1</b>");
-      //replace single *Word* with italics
-      newText = newText.replace(/\*(.*?)\*/g, "<i>$1</i>");
 
-      const newHeaderContent = {
-        tag: "h6",
-        text: prompt,
-      };
-      const newChatContents = [newHeaderContent];
-      const newContent = newText.split("\n");
-      newContent.forEach((p) => {
-        const newChatContent = {
-          tag: "p",
-          text: p,
-        };
-        newChatContents.push(newChatContent);
-      });
+      if (fullResponseText === "") {
+        setFullResponseText("### " + prompt + "\n\n" + newText);
+      } else {
+        setFullResponseText(
+          (prevText) => prevText + " \n\n### " + prompt + "\n\n" + newText
+        );
+      }
 
-      setChatContents((prev) => [...prev, ...newChatContents]);
       setPrompt("");
     } catch (e) {
-      const newChatContents = [
-        {
-          tag: "h6",
-          text: "There was an error:",
-        },
-        {
-          tag: "p",
-          text: e.message,
-        },
-        {
-          tag: "p",
-          text: e.name,
-        },
-        {
-          tag: "p",
-          text: e.stack,
-        },
-      ];
-      setChatContents((prev) => [...prev, ...newChatContents]);
+      if (fullResponseText === "") {
+        setFullResponseText("## There was an error: \n\n" + newText);
+      } else {
+        setFullResponseText(
+          (prevText) => prevText + " \n\n## There was an error: \n\n" + newText
+        );
+      }
     }
 
+    //Hide thinking
     containerRef.current.classList.replace("hidden", "special-flex");
     thinkingRef.current.classList.replace("flex", "hidden");
   }
@@ -139,65 +120,93 @@ export default function AI() {
         ref={containerRef}
         className="special-flex max-w-[1000px] m-auto h-full overflow-y-scroll"
       >
-        {chatContents.map((obj, index) => {
-          if (obj.tag === "h6") {
-            if (index === 0) {
-              return (
-                <p className="text-5xl" key={index}>
-                  {obj.text}
-                </p>
+        <ReactMarkdown
+          components={{
+            code(props) {
+              const { children, className, node, ...rest } = props;
+              const match = /language-(\w+)/.exec(className || "");
+
+              return match ? (
+                <>
+                  <SyntaxHighlighter
+                    {...rest}
+                    children={String(children).replace(/\n$/, "")}
+                    language={match[1]}
+                    style={dracula}
+                    PreTag={({ ...props }) => {
+                      return (
+                        <div {...props}>
+                          <div className="code-header flex">
+                            <div className="inline-block text-left w-full">
+                              {match[1]}
+                            </div>
+                            <div className="inline-block text-right w-full ">
+                              <button
+                                type="button"
+                                className="btn"
+                                onClick={(e) => {
+                                    const button = e.target;
+                                    navigator.clipboard.writeText(String(children).replace(/\n$/, ""));
+                                    button.innerHTML = "Copied";
+                                    setTimeout(() => {
+                                        button.innerHTML = "Copy";
+                                    }, 1000);
+                                }}
+                              >
+                                Copy
+                              </button>
+                            </div>
+                          </div>
+                          {children}
+                        </div>
+                      );
+                    }}
+                  />
+                </>
+              ) : (
+                <code {...rest} className={className}>
+                  {children}
+                </code>
               );
-            } else {
-              return (
-                <p className="text-5xl mt-[20px]" key={index}>
-                  {obj.text}
-                </p>
-              );
-            }
-          } else if (obj.text === "") {
-            return (
-              <p key={index} className="mt-[20px]">
-                {obj.text}
-              </p>
-            );
-          } else {
-            return (
-              <p key={index} dangerouslySetInnerHTML={{ __html: obj.text }} />
-            );
-          }
-        })}
+            },
+          }}
+        >
+          {fullResponseText}
+        </ReactMarkdown>
       </div>
-      <div className="flex-none h-[80px]">
-        <div className="flex gap-1 justify-center">
-          <details className="dropdown dropdown-top">
-            <summary className="btn">Google AI</summary>
-            <ul className="menu dropdown-content bg-base-200 rounded-box z-1 w-52 p-2 shadow-sm">
-              <li>
-                <a>Google AI</a>
-              </li>
-            </ul>
-          </details>
-          <input
-            type="password"
-            className="input w-[140px]"
-            required
-            placeholder="Enter an API Key"
-            onChange={(e) => setApiKey(e.target.value)}
-            value={apiKey}
-          />
-          <input
-            type="text"
-            className="input"
-            required
-            placeholder="Enter a Prompt"
-            onChange={(e) => setPrompt(e.target.value)}
-            value={prompt}
-          />
-          <button type="submit" className="btn" onClick={(e) => main()}>
-            Submit
-          </button>
+      <form onSubmit={handleSubmit}>
+        <div className="flex-none h-[80px]">
+          <div className="flex gap-1 justify-center">
+            <details className="dropdown dropdown-top">
+              <summary className="btn">Google AI</summary>
+              <ul className="menu dropdown-content bg-base-200 rounded-box z-1 w-52 p-2 shadow-sm">
+                <li>
+                  <a>Google AI</a>
+                </li>
+              </ul>
+            </details>
+            <input
+              type="password"
+              className="input w-[140px]"
+              required
+              placeholder="Enter an API Key"
+              onChange={(e) => setApiKey(e.target.value)}
+              value={apiKey}
+            />
+            <input
+              type="text"
+              className="input"
+              required
+              placeholder="Enter a Prompt"
+              onChange={(e) => setPrompt(e.target.value)}
+              value={prompt}
+            />
+            <button type="submit" className="btn">
+              Submit
+            </button>
+          </div>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
